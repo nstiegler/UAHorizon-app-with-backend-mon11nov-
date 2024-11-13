d0dd9ef16af1ee90bb1db660b1b4051e79c5d6ba
nstiegler <norman.stiegleriii@gmail.com>
2024-11-13 07:08:30 -0600

    Added the authorization rules for each table

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,34 +3,49 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 const schema = a.schema({
-  Jobs: a.model({
-    JobID: a.id(),
-    CustomerID: a.string(),
-    JobStatus: a.string(),
-    ScheduledDate: a.string(),
-    TechnicianID: a.string(),
-    ServiceType: a.string(),
-    Priority: a.string(),
-    CreatedAt: a.string(),
-  }),
+  Jobs: a
+    .model({
+      JobID: a.id(),
+      CustomerID: a.string(),
+      JobStatus: a.string(),
+      ScheduledDate: a.string(),
+      TechnicianID: a.string(),
+      ServiceType: a.string(),
+      Priority: a.string(),
+      CreatedAt: a.string(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
 
-  Technicians: a.model({
-    TechnicianID: a.id(),
-    Name: a.string(),
-    Skills: a.enum(["GR1", "GR2", "GR4", "ESHS", "ICH", "PINT", "MAP", "NDT"]),
-    Availability: a.boolean(),
-    AssignedJobID: a.string(), // Changed to nullable
-    Rating: a.float(),
-  }),
+  Technicians: a
+    .model({
+      TechnicianID: a.id(),
+      Name: a.string(),
+      Skills: a.enum([
+        "GR1",
+        "GR2",
+        "GR4",
+        "ESHS",
+        "ICH",
+        "PINT",
+        "MAP",
+        "NDT",
+      ]),
+      Availability: a.boolean(),
+      AssignedJobID: a.string(), // Changed to nullable
+      Rating: a.float(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
 
-  JobAssignments: a.model({
-    AssignmentID: a.id(),
-    JobID: a.string(),
-    TechnicianID: a.string(),
-    AssignedDate: a.string(),
-    CompletionStatus: a.string(),
-    CompletionDate: a.string(),
-  }),
+  JobAssignments: a
+    .model({
+      AssignmentID: a.id(),
+      JobID: a.string(),
+      TechnicianID: a.string(),
+      AssignedDate: a.string(),
+      CompletionStatus: a.string(),
+      CompletionDate: a.string(),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
